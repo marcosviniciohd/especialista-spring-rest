5dcbf51f61ac7842d73626372761fb65eac5cf63
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2023-01-06 11:58:13 -0300

    Ambiguidade de Beans e Injeção de lista de Beans

---
+++ injecao-depedencia/algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificadorSMS.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood.notificacao;
+
+import com.algaworks.algafood.modelo.Cliente;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificadorSMS implements Notificador {
+
+    @Override
+    public void notificar(Cliente cliente, String mensagem){
+        System.out.printf("Notificando %s através do SMS %s: %s\n", cliente.getNome(), cliente.getTelefone(), mensagem);
+    }
+}
--- injecao-depedencia/algafood-api/src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
+++ injecao-depedencia/algafood-api/src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
@@ -6,19 +6,20 @@ import com.algaworks.algafood.notificacao.NotificadorEmail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class AtivacaoClienteService {
 
-    @Autowired(required = false)
-    private Notificador notificador;
+    @Autowired
+    private List<Notificador> notificadores;
 
     public void ativar(Cliente cliente){
         cliente.ativar();
-        if (notificador != null){
-            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-        } else {
-            System.out.println("Não existe notificador, mas cliente foi ativado.");
-        }
+            for (Notificador notificador : notificadores){
+                notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+            }
+
 
 
     }
