fdb97e3a2439e62be84ee30421b770ebcd366248
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2023-01-06 20:22:53 -0300

    2.20 - Mudando o comportamento da aplicação com spring profile

--- injecao-depedencia/algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
+++ injecao-depedencia/algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -3,10 +3,16 @@ package com.algaworks.algafood.notificacao;
 import com.algaworks.algafood.modelo.Cliente;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+@Profile("prod")
 @TipoDoNotificador(NivelUrgencia.URGENTE)
 @Component
 public class NotificadorEmail implements Notificador {
+
+    public NotificadorEmail() {
+        System.out.println("NotificadorEmail REAL");
+    }
 
     @Override
     public void notificar(Cliente cliente, String mensagem){
---
+++ injecao-depedencia/algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificadorEmailMock.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafood.notificacao;
+
+import com.algaworks.algafood.modelo.Cliente;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+@Profile("dev")
+@TipoDoNotificador(NivelUrgencia.URGENTE)
+@Component
+public class NotificadorEmailMock implements Notificador {
+
+    public NotificadorEmailMock() {
+        System.out.println("NotificadorMock: MOCKADO");
+    }
+
+    @Override
+    public void notificar(Cliente cliente, String mensagem){
+        System.out.printf("MOCK: Notificação seria enviada para %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
+    }
+}
