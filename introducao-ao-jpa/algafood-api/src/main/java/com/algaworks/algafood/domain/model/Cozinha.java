509d2e2feac2d1d2cdf0a3d67039d9b2b6a65459
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2023-01-07 15:06:02 -0300

    03.17 - Conhecendo e usando o Lombok

---
+++ introducao-ao-jpa/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,38 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Cozinha {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+		Cozinha cozinha = (Cozinha) o;
+		return id != null && Objects.equals(id, cozinha.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return getClass().hashCode();
+	}
+}
---
+++ introducao-ao-jpa/algafood-api/src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
@@ -0,0 +1,26 @@
+package com.algaworks.algafood.jpa;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class BuscaCozinhaMain {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+
+		CozinhaRepository cozinhaRepository = applicationContext.getBean(CozinhaRepository.class);
+
+
+		Cozinha cozinha = cozinhaRepository.porId(1L);
+		System.out.println(cozinha.getNome());
+	}
+	
+}
