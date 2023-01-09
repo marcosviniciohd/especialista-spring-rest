4050bf9a970f973aa4554e8b01fd87bc4b8a4b1d
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2023-01-09 17:50:33 -0300

    Consultando Singleton Resource com com GET e @PathVariable

--- rest-com-spring/algafood-api/src/main/java/com/algaworks/algafood/controller/ControllerEstado.java
+++ rest-com-spring/algafood-api/src/main/java/com/algaworks/algafood/controller/ControllerEstado.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/estado")
+@RequestMapping("/estados")
 public class ControllerEstado {
 
     @Autowired
--- rest-com-spring/algafood-api/src/main/java/com/algaworks/algafood/controller/CozinhaController.java
+++ rest-com-spring/algafood-api/src/main/java/com/algaworks/algafood/controller/CozinhaController.java
@@ -5,12 +5,13 @@ import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 @RestController
-@RequestMapping("/cozinha")
+@RequestMapping("/cozinhas")
 public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
@@ -18,5 +19,14 @@ public class CozinhaController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Cozinha> listar(){
         return cozinhaRepository.todas();
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable Long cozinhaId){
+        System.out.println("------------------------------------------------------");
+        System.out.println(cozinhaId);
+        System.out.println("------------------------------------------------------");
+        return cozinhaRepository.porId(cozinhaId);
+
     }
 }
