ff5c715239c0275bd90b0088967402c89387299a
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2023-01-09 12:56:16 -0300

    04.13 - Implementando context negotiaton para retornar JSON ou XML

--- rest-com-spring/algafood-api/src/main/java/com/algaworks/algafood/controller/CozinhaController.java
+++ rest-com-spring/algafood-api/src/main/java/com/algaworks/algafood/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.controller;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Cozinha> listar(){
         return cozinhaRepository.todas();
     }
